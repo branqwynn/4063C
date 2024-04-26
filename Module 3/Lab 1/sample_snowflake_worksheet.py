6aa196bd472d187108b946003558a88571bbdc1e
branqwynn <70345762+branqwynn@users.noreply.github.com>
2024-04-25 22:26:56 -0400

    Delete Module 3/Lab 1 directory

--- Module 3/Lab 1/sample_snowflake_worksheet.py
+++
@@ -1,9 +0,0 @@
-import snowflake.snowpark as snowpark
-from snowflake.snowpark.functions import col
-
-def main(session: snowpark.Session): 
-    # Your code goes here, inside the "main" handler.
-    output = session.sql("create or replace temp stage my_stage").collect()
-
-    # Return value will appear in the Results tab.
-    return output
